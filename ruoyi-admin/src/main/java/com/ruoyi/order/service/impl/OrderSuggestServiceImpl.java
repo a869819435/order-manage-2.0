25e10b4233bfe8dba65d6c86fc3b386db678bb28
ywq <869819435@qq.com>
2021-04-14 18:01:11 +0800

    feat: code

--- ruoyi-admin/src/main/java/com/ruoyi/order/service/impl/OrderSuggestServiceImpl.java
+++ ruoyi-admin/src/main/java/com/ruoyi/order/service/impl/OrderSuggestServiceImpl.java
@@ -2,9 +2,11 @@ package com.ruoyi.order.service.impl;
 
 import com.ruoyi.address.domain.AddressInfo;
 import com.ruoyi.order.domain.OrderSuggestInfoVO;
+import com.ruoyi.order.service.IOrderIncomeInfoService;
 import com.ruoyi.order.service.OrderSuggestService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -12,6 +14,8 @@ import java.util.List;
 @Service
 public class OrderSuggestServiceImpl implements OrderSuggestService {
 
+    @Resource
+    private IOrderIncomeInfoService iOrderIncomeInfoService;
 
     @Override
     public List<OrderSuggestInfoVO> getOrderSuggestOfStart(AddressInfo start, BigDecimal distance, String type) {
